cbfc893ff68d595d1ca310771a5f69e7253521ac
eborszem <eborszem@purdue.edu>
2025-07-12 00:53:24 -1000

    Remove unused code

--- website/demo/src/main/java/com/noteproject/demo/Controller/HomeController.java
+++ website/demo/src/main/java/com/noteproject/demo/Controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.noteproject.demo.Controller;
 
-import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
--- website/demo/src/main/java/com/noteproject/demo/Model/Measure.java
+++ website/demo/src/main/java/com/noteproject/demo/Model/Measure.java
@@ -1,14 +1,11 @@
 package com.noteproject.demo.Model;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Measure {
     Chord chord; // chords objects in the measure
     public Measure next;
 	private int noteValue; // which note gets the beat. Quarter, half, etc.
 	private int numNoteValuesPerMeasure;  // how many note beats are present in the measure
-	private int numBeats = noteValue * numNoteValuesPerMeasure;
+	// private int numBeats = noteValue * numNoteValuesPerMeasure;
     private int id;
     private int measureNumber;
     private int compositionId;
@@ -22,24 +19,6 @@ public class Measure {
     public Measure() {
         
     }
-
-    // note to self: this function moves the heads of the lists, resulting in them becoming null after this is called
-    public void printMeasure(Measure m) {
-        while (m.chord != null) {
-            int duration = m.chord.note.duration;
-            while (m.chord.note != null) {
-                m.chord.note = m.chord.note.next;
-            }
-            System.out.print(":" + duration + "\n");
-            m.chord = m.chord.next;
-        }
-    }
-
-    public Measure(int noteValue, int numNoteValuesPerMeasure) {
-        this.noteValue = noteValue;
-        this.numNoteValuesPerMeasure = numNoteValuesPerMeasure;
-    }
-    
 
     public Chord getChord() {
         return this.chord;
@@ -71,28 +50,6 @@ public class Measure {
 
     public void setNumNoteValuesPerMeasure(int numNoteValuesPerMeasure) {
         this.numNoteValuesPerMeasure = numNoteValuesPerMeasure;
-    }
-
-    public List<Chord> getAllChords() {
-        List<Chord> chords = new ArrayList<>();
-        Chord c = this.chord;
-        while (c != null) {
-            chords.add(c);
-            c = c.next;
-        }
-        return chords;
-    }
-
-    // All measures can be accessed linearly via the first measure in the composition
-    public List<Measure> getAllMeasures() {
-        List<Measure> measures = new ArrayList<>();
-        Measure m = this;
-        while (m != null) {
-            measures.add(m);
-            m = m.next;
-        }
-        System.out.println("measure object val="+this);
-        return measures;
     }
 
     public int getId() {
